2a24b3417d32cae6dfd1f1ea44fa4e2625a9e3c7
Yoonmee <ymhwang119@naver.com>
2017-01-23 17:29:03 +0900

    header 완성

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset = "utf-8">
+  <title> 블로그 홈페이지 </title>
+  <link rel="stylesheet" type="text/css" href="css/style.css">
+  <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+</head>
+<body>
+<header>
+  <img id="headerImage" src = "images/blog_header-black.png" alt="Blog header image">
+</header>
+<nav>
+<ul>
+  <li> <a class="selected" href="index.php">HOME</a></li>
+  <li> <a href="setting.php"> SETTING </a> </li>
+
+  <li>
+    <a class = "topRight" href="Join.php">
+    <i class="fa fa-user-circle-o" aria-hidden="true"></i>
+     Sign Up </a> </li>
+
+  <li>
+    <a class = "topRight" href="Login.php">
+    <i class="fa fa-sign-in" aria-hidden="true"></i>
+     Login </a> </li>
+</ul>
+</nav>
+<footer>
+   Copyright &copy; 2017 YoonWook. All rights reserved.
+</footer>
+</body>
+</html>
