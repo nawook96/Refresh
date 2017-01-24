8d0571a6e981842ca76e9f44a5f24fdb2a2303d8
Yoonmee <ymhwang119@naver.com>
2017-01-24 13:04:52 +0900

    흐음?

--- index.php
+++ index.php
@@ -9,7 +9,25 @@
 <body>
 <? include("frame/header.php")?>
 <? include("frame/navbar.php")?>
-
+<div id = "allcontent">
+  <aside>
+    <div id="profileBox">
+      <img id="profileImage" src = "images/profile_image.png" alt="Blog Profile image">
+      <p> <span class="title"> Name </span> <span class="title_content" name="name" > Peter </span> </p>
+      <p> <span class="title"> Intro </span> <span class="title_content" name="intro" > Hi I am Peter </span> </p>
+    </div>
+    <div id="sidebar">
+      <ul>
+        <li> 공지사항 </li>
+        <li> 잡담 </li>
+        <li> 공부 </li>
+      </ul>
+    </div>
+  </aside>
+  <article>
+    여기는 텍스트입니다.
+  </article>
+</div>
 <?include("frame/footer.php")?>
 </body>
 </html>
