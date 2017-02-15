c9e30dfd1d4384a66ae08ab54f392a41c36f82c3
Dongwook Shin <nawook0331@nate.com>
2017-02-15 14:16:24 +0900

    ddd

--- profileset.php
+++ profileset.php
@@ -18,7 +18,11 @@ $adrow = $adresult->fetch_assoc();
   <script type="text/javascript" src="js/profileset.js"></script>
 </head>
 <body>
-  <div>
+  	<?php include("frame/header.php");?>
+  	<?php include("frame/navbar.php");?>
+    <div class = "allcontent">
+      <?php include("frame/aside_user.php"); ?>
+    <article class="boardArticle">
     <form name="joinfrm" id="joinfrm" action="profile.php" method="post">
   <div class ="wrapper_table">
    <div class="join_body">
@@ -31,6 +35,12 @@ $adrow = $adresult->fetch_assoc();
       <dt><label>Intro</label></dt>
       <TEXTAREA style = "width:400px"name="ad_intro" cols=90 rows=15><?php echo isset($adrow['ad_intro'])?$adrow['ad_intro']:null?></TEXTAREA>
      </div>
+     <div>
+       <input type="hidden" name=MAX_FILE_SIZE value="<?=$fsize?>">
+         파일 용량 제한 : 3MB
+         <input type="checkbox" name="check" value="noImage">이미지 첨부 삭제&nbsp;&nbsp;&nbsp;&nbsp;
+           <input  type="file" name="fileName" accept="image/gif, image/jpeg, image/png">
+     </div>
     </dl>
    </div>
    <div class="join_footer">
@@ -40,6 +50,9 @@ $adrow = $adresult->fetch_assoc();
   </form>
 
   </div>
-</body>
+</article>
+  <?php include("frame/footer.php");?>
+  </body>
+
 
 </html>
